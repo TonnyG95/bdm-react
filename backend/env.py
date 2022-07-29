898332f43ddfa79c8accafbd345fc5bdddfd7abb
TonnyG95 <galic_antonio@hotmail.com>
2022-07-29 13:09:35 +0000

    started working on backend

--- backend/env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["DATABASE_URL"] = "postgres://rpyliyejblqspz:3d0c09e476f73147c8f0c8b568f4c44e5ee305d2d50d167f17b0d45b3fc0ca20@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d73m057l7bjh9a"
-os.environ["CLOUDINARY_URL"] = "cloudinary://784682421534367:bWWnVgea_HIlrsn0R-TL2iKz80M@bdm"
-os.environ["SECRET_KEY"] = "SecretKey!"
