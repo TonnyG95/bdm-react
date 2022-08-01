9962a7a144f1f3a0a2cd9609ccb0280dabc38095
TonnyG95 <galic_antonio@hotmail.com>
2022-08-01 14:57:34 +0000

    added pillow and urls to load images

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 
 # Default primary key field type
